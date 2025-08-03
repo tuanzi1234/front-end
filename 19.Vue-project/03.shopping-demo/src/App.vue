caa0d715f2186bcc4fae6b3a9d6694eb15ca3cd6
tuanzi <lvbohan2003@163.com>
2025-08-04 03:03:40 +0800

    项目初始化

---
+++ 19.Vue-project/03.shopping-demo/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <!-- 测试Vant 组件 -->
+    <van-button type="danger">按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="less">
+</style>
