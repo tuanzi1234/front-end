6045f697e0ca2d58c9831ffb531db6da615238c8
tuanzi <lvbohan2003@163.com>
2025-08-26 02:22:20 +0800

    微信小程序开发暂停

--- 20.WeChat/uni-shopping/main.js
+++ 20.WeChat/uni-shopping/main.js
@@ -1,22 +1,43 @@
 import App from './App'
 
+// 导入网络请求
+import {$http} from "@escook/request-miniprogram"
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
+
+// 挂载$http
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net/'
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 创建Vue实例
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-  ...App
+    ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {createSSRApp} from 'vue'
+
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+    const app = createSSRApp(App)
+    return {
+        app
+    }
 }
+
 // #endif
--- 20.WeChat/uni-shopping/pages/home/home.vue
+++ 20.WeChat/uni-shopping/pages/home/home.vue
@@ -1,18 +1,23 @@
+<script setup>
+import { onMounted } from "vue";
+
+// 轮播图数据
+const swiperList = []
+
+// 获取轮播图数据
+
+
+onMounted(() => {
+  // 获取轮播图数据
+  getSwiperList()
+})
+</script>
+
 <template>
 	<view>
 		home
 	</view>
 </template>
-
-<script>
-	export default {
-		data() {
-			return {
-
-			};
-		}
-	}
-</script>
 
 <style lang="scss">
 
