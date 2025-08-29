bb11d3f18c06938249012c29799046a436ab97d1
tuanzi <lvbohan2003@163.com>
2025-08-30 03:08:10 +0800

    react评论案例重新封装

--- 21.React/01.EasyReact/react-basic/src/App.js
+++ 21.React/01.EasyReact/react-basic/src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import avatar from './images/bozai.png';
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {orderBy} from "lodash";
 import classNames from "classnames/dedupe";
 import {v4 as uuidv4} from 'uuid';
 import dayjs from "dayjs";
+import axios from "axios";
 
 /**
  * 评论列表的渲染和操作
@@ -14,23 +15,7 @@ import dayjs from "dayjs";
  */
 
 // 评论列表数据
-const defaultList = [{
-    // 评论id
-    rpid: 3, // 用户信息
-    user: {
-        uid: '13258165', avatar: '', uname: '周杰伦',
-    }, // 评论内容
-    content: '哎哟，不错哦', // 评论时间
-    ctime: '10-18 08:15', like: 88,
-}, {
-    rpid: 2, user: {
-        uid: '36080105', avatar: '', uname: '许嵩',
-    }, content: '我寻你千百度 日出到迟暮', ctime: '11-13 11:29', like: 88,
-}, {
-    rpid: 1, user: {
-        uid: '30009257', avatar, uname: '黑马前端',
-    }, content: '学前端就来黑马', ctime: '10-19 09:00', like: 66,
-},]
+const defaultList = []
 // 当前登录用户信息
 const user = {
     // 用户id
@@ -50,10 +35,66 @@ const user = {
 
 // 导航 Tab 数组
 const tabs = [{type: 'hot', text: '最热'}, {type: 'time', text: '最新'},]
+// 封装请求数据的hooks
+const useGetList = () => {
+    // 使用useState维护list
+    // const [commentList, setCommentList] = useState(orderBy(defaultList, 'like', 'desc'))
+    // 获取接口数据渲染
+    const [commentList, setCommentList] = useState([])
+    useEffect(() => {
+        async function getList() {
+            const res = await axios.get('http://localhost:3004/list')
+            setCommentList(res.data)
+        }
+
+        // 获取接口数据
+        getList()
+    }, [])
+    return {
+        commentList,
+        setCommentList
+    }
+}
+
+// 封装组件item
+function Item({item, onDelete}) {
+    return (<div className="reply-item">
+        {/* 头像 */}
+        <div className="root-reply-avatar">
+            <div className="bili-avatar">
+                <img
+                    className="bili-avatar-img"
+                    alt=""
+                    src={item.user.avatar}
+                />
+            </div>
+        </div>
+
+        <div className="content-wrap">
+            {/* 用户名 */}
+            <div className="user-info">
+                <div className="user-name">{item.user.uname}</div>
+            </div>
+            {/* 评论内容 */}
+            <div className="root-reply">
+                <span className="reply-content">{item.content}</span>
+                <div className="reply-info">
+                    {/* 评论时间 */}
+                    <span className="reply-time">{item.ctime}</span>
+                    {/* 评论数量 */}
+                    <span className="reply-time">点赞数:{item.like}</span>
+                    {user.uid === item.user.uid &&
+                        <span className="delete-btn" onClick={() => onDelete(item.rpid)}>删除</span>
+                    }
+                </div>
+            </div>
+        </div>
+    </div>)
+}
 
 const App = () => {
-    // 使用useState维护list
-    const [commentList, setCommentList] = useState(orderBy(defaultList, 'like', 'desc'))
+    // 从hooks中解构数据
+    const {commentList, setCommentList} = useGetList()
     const handleDelete = (rpid) => {
         // 删除评论
         setCommentList(commentList.filter((item) => item.rpid !== rpid))
@@ -152,38 +193,7 @@ const App = () => {
                 {/* 评论列表 */}
                 <div className="reply-list">
                     {/* 评论项 */}
-                    {commentList.map((item) => (<div key={item.rpid} className="reply-item">
-                        {/* 头像 */}
-                        <div className="root-reply-avatar">
-                            <div className="bili-avatar">
-                                <img
-                                    className="bili-avatar-img"
-                                    alt=""
-                                    src={item.user.avatar}
-                                />
-                            </div>
-                        </div>
-
-                        <div className="content-wrap">
-                            {/* 用户名 */}
-                            <div className="user-info">
-                                <div className="user-name">{item.user.uname}</div>
-                            </div>
-                            {/* 评论内容 */}
-                            <div className="root-reply">
-                                <span className="reply-content">{item.content}</span>
-                                <div className="reply-info">
-                                    {/* 评论时间 */}
-                                    <span className="reply-time">{item.ctime}</span>
-                                    {/* 评论数量 */}
-                                    <span className="reply-time">点赞数:{item.like}</span>
-                                    {user.uid === item.user.uid &&
-                                        <span className="delete-btn" onClick={() => handleDelete(item.rpid)}>删除</span>
-                                    }
-                                </div>
-                            </div>
-                        </div>
-                    </div>))}
+                    {commentList.map((item) => (<Item key={item.id} item={item} onDelete={handleDelete} />))}
                 </div>
             </div>
         </div>)
